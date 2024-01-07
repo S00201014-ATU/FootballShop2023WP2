79a5b18bc0ea737455085237e56556c7bbf1c31c
S00201014-ATU <S00201014@atu.ie>
2024-01-07 18:34:20 +0000

    Add code to do with login

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -5,3 +5,5 @@ export const KITS_URL = BASE_URL + '/api/kits';
 export const KITSS_BY_SEARCH_URL = KITS_URL + '/search/';
 
 export const Kit_BY_ID_URL = KITS_URL + '/';
+
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
